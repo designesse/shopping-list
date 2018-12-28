9b03ae197a4b597e65163dd19aae0d8e7eb6e9e8
Malina Randrianavony <designesse@malinas-imac.lan>
2018-12-27 23:23:23 -0700

    Updates

---
+++ precache-manifest.7aa18641a49c90402f8213cd8c9c6e56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cf887962af3fa01d448",
+    "url": "/shopping-list/static/css/main.be8067c4.chunk.css"
+  },
+  {
+    "revision": "4cf887962af3fa01d448",
+    "url": "/shopping-list/static/js/main.4cf88796.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/shopping-list/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "b61658b2664bfbbcf66f",
+    "url": "/shopping-list/static/js/runtime~main.b61658b2.js"
+  },
+  {
+    "revision": "d2336678c85de05649d01905c7528ba8",
+    "url": "/shopping-list/index.html"
+  }
+];
